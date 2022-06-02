245ce82ee1a885583866d80a4aaef3cff3717b5a
akarsid <siddharth.akar@qoala.id>
2022-06-02 21:14:13 +0530

    updated

--- book.go
+++ book.go
@@ -83,22 +83,6 @@ func GetAuthors(w http.ResponseWriter, r *http.Request){
 	}
 	json.NewEncoder(w).Encode(authors)
 }
-// func UpdateUser(w http.ResponseWriter, r *http.Request){
-// 	w.Header().Set("Content-Type", "application/json")
-// 	params:=mux.Vars(r)
-// 	var user []User
-// 	DB.First(&user,params["id"])
-// 	json.NewDecoder(r.Body).Decode(&user)
-// 	DB.Save(&user)
-// 	json.NewEncoder(w).Encode(user)
-// }
-// func DeleteUser(w http.ResponseWriter, r *http.Request){
-// 	w.Header().Set("Content-Type", "application/json")
-// 	params:=mux.Vars(r)
-// 	var user []User
-// 	DB.Delete(&user,params["id"])
-// 	json.NewEncoder(w).Encode("user is deleted successfully")
-// }
 
 
 
